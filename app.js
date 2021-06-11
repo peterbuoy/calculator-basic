f2a10e05bbcfb43059516c7547551d8495976752
peterbuoy <peterbuoy@gmail.com>
2021-06-11 15:52:33 -0700

    support for floats and added error checking for decimals and repeated operations

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const DELETE_BUTTON = document.getElementById('button_DEL');
 const NUMBER_BUTTONS = document.querySelectorAll('.number > button');
 const OP_BUTTONS = document.querySelectorAll('.operator > button');
 const EQUAL_BUTTON = document.querySelector('#button_equal');
+const DECIMAL_BUTTON = document.querySelector('#button_decimal');
+const ALL_BUTTONS = document.querySelectorAll('button')
 
-let displayNum = "0";
+OP_BUTTONS.forEach(button => button.disabled = true);
+EQUAL_BUTTON.disabled = true;
 let currentNum = "";
 let previousNum = "";
 let previousOp = "";
 
 const printState = () => {
   console.log('_Variable state_');
-  console.log(`previousNum is ${previousNum}`);
   console.log(`currentNum is ${currentNum}`);
+  console.log(`previousNum is ${previousNum}`);
   console.log(`previousOp is ${previousOp}`);
   console.log(' ');
 }
@@ -23,86 +26,112 @@ const updateDisplay = (num) => {
 }
 
 const clearDisplay = () => {
-  displayNum = "0";
   currentNum = "";
   previousNum = "";
   previousOp = "";
-  updateDisplay(displayNum);
+  updateDisplay("0");
+  ALL_BUTTONS.forEach(button => button.disabled = false);
+  disableOpsAndEquals();
   console.log('Display Cleared');
   printState();
 }
 
 // deletes previously inputted number and ensures display is never completely empty
 const deleteLast = () => {
-  if (displayNum == "0") return;
-  if (currentNum.length == 1 && currentNum != "0") {
-    currentNum = "0";
-    displayNum = currentNum;
-    updateDisplay(displayNum);
+  if (currentNum.length == 1 || currentNum == "") {
+    currentNum = ""
+    updateDisplay("0");
+    printState();
   } else {
     currentNum = currentNum.substring(0, currentNum.length - 1);
-    displayNum = currentNum;
-    updateDisplay(displayNum);
+    updateDisplay(currentNum);
+    printState();
   }
 }
 
 const clickNumber = (event) => {
   const buttonValue = event.target.value;
-  // prevents adding number to 0
-  if (displayNum == "0" || currentNum == "0") {
-    console.log('clickNumber set currentNum to empty!');
-    currentNum = "";
+  if (DISPLAY_P.textContent == "0" && buttonValue == "." 
+      || (DISPLAY_P.textContent.includes(".") && buttonValue == ".")) {
+    alert('Please enter decimals in valid areas.');
+    return;
   }
   currentNum += buttonValue;
-  displayNum = currentNum;
+  updateDisplay(currentNum);
   printState();
-  updateDisplay(displayNum);
 }
 
 const operations = {
-  add: (a, b) => Number(a) + Number(b),
-  subtract: (a, b) => Number(a) - Number(b),
-  multiply: (a, b) => Number(a) * Number(b),
-  divide: (a, b) => Number(a) / Number(b)
+  add: (a, b) => parseFloat(Number(a) + Number(b)),
+  subtract: (a, b) => parseFloat(Number(a) - Number(b)),
+  multiply: (a, b) => parseFloat(Number(a) * Number(b)),
+  divide: (a, b) => {
+    if (Number(b) == 0) {
+      alert("Can't divide by zero!")
+      clearDisplay();
+      return;
+    };
+    return parseFloat(Number(a) / Number(b));
+  }
 }
 
 const compute = (event) => {
-  console.log('inside compute');
   const opValue = event.target.value;
   console.log(opValue);
-  console.log(`previousOp is ${previousOp}`);
-  console.log(' ');
   // first operation
   if(previousNum == "") {
     previousNum = currentNum;
-    currentNum = "0";
-    displayNum = currentNum;
-    updateDisplay(currentNum);
+    currentNum = "";
+    updateDisplay("0");
     printState();
   // subsequent operations
   } else {
-    console.log('inside else');
-    console.log(previousOp);
-    console.log(previousNum);
-    console.log(currentNum);
     previousNum = operations[previousOp](previousNum, currentNum);
-    currentNum = "0";
+    currentNum = "";
     updateDisplay(previousNum);
     printState();
   }
   previousOp = opValue;
 };
 
-const evaluate = (event) => {
-  previousNum = operations[previousOp](previousNum, currentNum);
-  currentNum = "";
+const evaluate = () => {
+  currentNum = operations[previousOp](previousNum, currentNum);
+  previousNum = "";
   previousOp = "";
+  updateDisplay(currentNum);
   printState();
-  updateDisplay(previousNum);
+}
+
+const disableEqualsAndNumbers = () => {
+  EQUAL_BUTTON.disabled = true;
+  NUMBER_BUTTONS.forEach(button => button.disabled = true);
+}
+
+const disableOpsAndEquals = () => {
+  EQUAL_BUTTON.disabled = true;
+  OP_BUTTONS.forEach(button => button.disabled = true);
+}
+
+// const disableDecimal = () => {
+//   DECIMAL_BUTTON.disabled = true;
+// }
+
+const enableOpsAndEquals = () => {
+  EQUAL_BUTTON.disabled = false;
+  OP_BUTTONS.forEach(button => button.disabled = false);
+}
+
+const enableNums = () => {
+  NUMBER_BUTTONS.forEach(button => button.disabled = false);
 }
 
 CLEAR_BUTTON.addEventListener('click', clearDisplay);
 DELETE_BUTTON.addEventListener('click', deleteLast);
 NUMBER_BUTTONS.forEach(button => button.addEventListener('click', clickNumber)); 
-OP_BUTTONS.forEach(button => button.addEventListener('click', compute)); 
+NUMBER_BUTTONS.forEach(button => button.addEventListener('click', enableOpsAndEquals));
+//DECIMAL_BUTTON.addEventListener('click', disableDecimal);
+OP_BUTTONS.forEach(button => button.addEventListener('click', compute));
+OP_BUTTONS.forEach(button => button.addEventListener('click', disableOpsAndEquals)); 
+OP_BUTTONS.forEach(button => button.addEventListener('click', enableNums)); 
 EQUAL_BUTTON.addEventListener('click', evaluate);
+EQUAL_BUTTON.addEventListener('click', disableEqualsAndNumbers);
